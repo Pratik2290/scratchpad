29c947fae8dd09abba6b9e7c2582cc12b0ba4a00
Pratik Bajaj <pratik.bajaj@aeratechnology.com>
2019-10-17 10:01:11 +0530

    Find Pair

--- src/main/java/com/test/code/arrays/FindPair.java
+++ src/main/java/com/test/code/arrays/FindPair.java
@@ -11,48 +11,36 @@ public class FindPair {
     public static void main(String[] args) {
 
         int[] array = {-1, 0, 1, 3, 5, 6};
-        int sumValue = 2;
+        int sumValue = 5;
 
         int count = findPair(array, sumValue);
         System.out.println(count);
     }
 
     private static int findPair(int[] array, int sumValue) {
-        int i = 0;
         int j = array.length - 1;
         int count = 0;
 
-        while (i <= j) {
-            int smaller = array[i];
-            int greater = array[j];
-
-            int sum = smaller + greater;
-
-            if (sum < sumValue) {
-                int x = i;
-                while (x < j) {
-                    if (sum < sumValue) {
-                        x++;
-                        sum = smaller + array[x];
-                    }
-                }
-                count = count + x - i;
-                i++;
-            } else if (sum > sumValue) {
-                int x = i;
-                x++;
-                while (sum > sumValue && x < j) {
-                    count++;
-                    x++;
-                    sum = greater + array[x];
-                }
-                j--;
-            } else {
-                i++;
-                j--;
+        while (j >= 0) {
+            int number = array[j];
+            int diff = sumValue - number;
+            int index = findfirstIndexGreaterThan(diff, array, j);
+            if (index != -1) {
+                count = count + j - index;
             }
+            j--;
         }
         return count;
+    }
+
+    private static int findfirstIndexGreaterThan(int diff, int[] array, int j) {
+        for (int i = 0; i < j; i++) {
+            int value = array[i];
+            if (value > diff) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
